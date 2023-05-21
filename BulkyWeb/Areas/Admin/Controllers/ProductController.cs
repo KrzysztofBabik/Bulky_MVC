bf8b8bdb579844964fb674e3ed9a308308d4280b
KrzysztofBabik <krzysztofbabik.kb@gmail.com>
2023-05-21 23:19:00 +0200

    Added dropdown list of categories in Product Edit Page

--- BulkyWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Bulky.DataAccess.Repository;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
@@ -18,25 +20,50 @@ namespace BulkyWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            
             return View(objProductList);
         }
 
         public IActionResult Create() 
         {
-            return View();
+            IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category
+                .GetAll().ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+
+            //ViewBag.CategoryList = CategoryList;          //It was only ViewBag example for practice.
+            //ViewData["CategoryList"] = CategoryList;      //It was only ViewData example for practice.
+            ProductVM productVM = new() 
+            { 
+                CategoryList = CategoryList,
+                Product = new Product()
+            };
+
+            return View(productVM);
         }
         [HttpPost]
-        public IActionResult Create(Product obj) 
+        public IActionResult Create(ProductVM productVM) 
         {
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Product.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Category created successfully!";
+
                 return RedirectToAction("Index");
             }
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().ToList().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
 
-            return View();
+                return View(productVM);
+            }
         }
         public IActionResult Edit(int? id)
         {
